7c0539fede5bf0872793228cb4dd63538e9dcf92
Tuukka Ervasti <tuukka.ervasti@gmail.com>
2021-09-06 18:34:42 +0300

    First commit to new repo

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import akeem from "./images/ak.jpg";
+import './App.css';
+
+function App() {
+  return (
+    <div className="bg">
+        <img src={akeem} className="akeem" alt="logo" />
+    </div>
+  );
+}
+
+export default App;
