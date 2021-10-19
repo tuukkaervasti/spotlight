0d05907976db065d248c481c8ee0066c2d843147
Tuukka Ervasti <tuukka.ervasti@gmail.com>
2021-10-19 11:44:02 +0300

    Added mongodb schema

---
+++ backend/models/PhotographedPeople.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const PhotographedPeopleSchema = new Schema({
+    personName: String,
+    date: Date,
+    photoArray: Array, 
+    closeupPhoto: Number,
+    fullbodyPhoto: Number,
+    quoteText: String,
+    videoUrl: String, 
+});
+
+const PhotographedPeople = mongoose.model("PhotographedPeople", PhotographedPeopleSchema);
+module.exports = PhotographedPeople;
