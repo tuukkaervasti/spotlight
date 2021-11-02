b1798878f9a75cc145b21ebf5deb7fedbeb1a71e
Tuukka Ervasti <tuukka.ervasti@gmail.com>
2021-11-02 11:17:11 +0200

    Added a grid component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,24 @@
 import akeem from "./images/ak.jpg";
 import './App.css';
 import { BrowserRouter as Router } from "react-router-dom";
+import Grid from "./Grid";
 
-function App() {
+/* function App() {
   return (
     <div className="bg">
         <img src={akeem} className="akeem" alt="logo" />
     </div>
   );
+} */
+
+const arrOfObj = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
+
+function App() {
+  return(
+    <div className="container">
+      <Grid arrOfObj={arrOfObj}/>
+    </div>
+  )
 }
 
 export default App;
