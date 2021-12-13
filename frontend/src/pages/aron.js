ccf8efc516812040122ba4f5b9fa03d071f83ac1
Tuukka Ervasti <tuukka.ervasti@gmail.com>
2021-12-13 16:22:52 +0000

    added text content to aron.js interview component props

--- frontend/src/pages/aron.js
+++ frontend/src/pages/aron.js
@@ -12,17 +12,28 @@ const Aron = () => {
         
         <div className="aron">
             <HeaderImage />
-            <Interview text="I'm here, boy!"/>
+            
+            <Interview text="I’m Aron, aka Akeem, a 27-year old music producer and studio technician from Eastern Helsinki. 
+            I’ve been doing music stuff for the past 7 or 8 years, day in day out."/>
+            
             <ReactPlayer className="reactPlayer" url="https://vimeo.com/646173014" 
             controls="true;" 
             width="1000px"
             height="563px"
-            
             />
-            <Interview text="And also here!"/>
+            
+            <Interview text="Aron gets up in the morning, Akeem goes to the studio, Akeem goes home from the studio, Aron goes to sleep. 
+            Akeem is the one who goes to the club for the gig and who produces the track, Aron is the one who my mom complains to 
+            
+            about not taking the dog out enough etc."/>
             <img className="juttuImage" src={Aron2}></img>
+            
             <Interview text="But don't forget me here!"/>
+            
             <img className="pystyImage" src={Aron3}></img>
+            
+            <Interview text="The last of me"/>
+            
             </div>
            
             
